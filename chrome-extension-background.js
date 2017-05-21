c366f99dbea5cc56f3d2d87375df8a29dc3ce917
Kulshekhar Kabra <kulshekhar@users.noreply.github.com>
2017-05-22 05:29:43 +0530

    Update chrome-extension-background.js

--- chrome-extension-background.js
+++ chrome-extension-background.js
@@ -1,7 +1,7 @@
     chrome.webRequest.onBeforeRequest.addListener((details) => {
 
       if (details.url.endsWith('.js') && details.url.indexOf('classroom.udacity.com/js/manifest.') >= 0) {
-        return { redirectUrl: 'https://cdn.rawgit.com/kulshekhar/udacity-content-fix/master/manifest-replacement.js' };
+        return { redirectUrl: 'https://cdn.rawgit.com/kulshekhar/udacity-content-fix/fcaeb037c5cf97e980c3b34b4ff8b5c00648e831/manifest-replacement.js' };
       }
 
       return details.url
